03119d5d6161124c18adc033850510591a8c79b3
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-02-12 17:19:41 +0700

    Connecting peers

---
+++ src/p2p.js
@@ -0,0 +1,35 @@
+const WebSockets = require("ws");
+
+const sockets = [];
+
+const startP2PServer = port => {
+  const server = new WebSockets.Server({ port });
+  server.on("connection", ws => {
+    initConnection(ws);
+  });
+  console.log(`Nomad Coin P2P Server Running on port ${port} ✅`);
+};
+
+const getSockets = () => sockets;
+
+const initConnection = socket => {
+  sockets.push(socket);
+  socket.on("message", () => {
+    console.log("welcome!");
+  });
+  setTimeout(() => {
+    socket.send("welcome");
+  }, 1000);
+};
+
+const connectToPeers = newPeer => {
+  const ws = new WebSockets(newPeer);
+  ws.on("open", () => {
+    initConnection(ws);
+  });
+};
+
+module.exports = {
+  startP2PServer,
+  connectToPeers
+};
--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ const express = require("express"),
   bodyParser = require("body-parser"),
   morgan = require("morgan"),
   Blockchain = require("./blockchain"),
-  ws = require("./ws");
+  P2P = require("./p2p");
 
 const getBlockchain = Blockchain.getBlockchain;
 const createNewBlock = Blockchain.createNewBlock;
+const connectToPeers = P2P.connectToPeers;
 
 const HTTP_PORT = process.env.HTTP_PORT || 3000;
 const P2P_PORT = process.env.P2P_PORT || 4000;
@@ -25,9 +26,14 @@ app.post("/mineBlock", (req, res) => {
   res.send(newBlock);
 });
 
+app.post("/addPeer", (req, res) => {
+  connectToPeers(req.body.peer);
+  res.send();
+});
+
 // export HTTP_PORT=
 app.listen(HTTP_PORT, () => {
   console.log(`Nomad Coin Node Running on port ${HTTP_PORT} ✅`);
 });
 
-ws.startP2PServer(P2P_PORT);
+P2P.startP2PServer(P2P_PORT);
