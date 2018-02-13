02ee34c1adf22d2b3c13e8d1a79a148c094a5131
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-02-13 14:24:03 +0700

    Eslint

--- src/blockchain.js
+++ src/blockchain.js
@@ -3,23 +3,23 @@ const CryptoJS = require("crypto-js");
 // Block Structure
 
 class Block {
-  constructor(index, hash, previousHash, timestamp, data) {
-    this.index = index;
-    this.hash = hash;
-    this.previousHash = previousHash;
-    this.timestamp = timestamp;
-    this.data = data;
-  }
+    constructor(index, hash, previousHash, timestamp, data) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+    }
 }
 
 // Hardcode the genesisBlock
 
 const genesisBlock = new Block(
-  0,
-  "3DF6EF422472827B1E77AD3E7A194108BBB4D8B925176AFCABE7BEDA9E561071",
-  null,
-  new Date().getTime() / 1000,
-  "Genesis block MF"
+    0,
+    "3DF6EF422472827B1E77AD3E7A194108BBB4D8B925176AFCABE7BEDA9E561071",
+    null,
+    new Date().getTime() / 1000,
+    "Genesis block MF"
 );
 
 // Create the blockchain with the Genesis Block hardcoded into it.
@@ -29,9 +29,9 @@ let blockchain = [genesisBlock];
 // Create the hash of the block
 
 const createHash = (index, previousHash, timestamp, data) =>
-  CryptoJS.SHA256(
-    index + previousHash + timestamp + JSON.stringify(data)
-  ).toString();
+    CryptoJS.SHA256(
+        index + previousHash + timestamp + JSON.stringify(data)
+    ).toString();
 
 // Get the last block from the blockchain
 const getNewestBlock = () => blockchain[blockchain.length - 1];
@@ -42,102 +42,102 @@ const getBlockchain = () => blockchain;
 // Create a new block
 
 const createNewBlock = data => {
-  const previousBlock = getNewestBlock();
-  const newBlockIndex = previousBlock.index + 1;
-  const newtimestamp = new Date().getTime() / 1000;
-  const newHash = createHash(
-    newBlockIndex,
-    previousBlock.hash,
-    newtimestamp,
-    data
-  );
-  const newBlock = new Block(
-    newBlockIndex,
-    newHash,
-    previousBlock.hash,
-    newtimestamp,
-    data
-  );
-  addBlockToChain(newBlock);
-  return newBlock;
+    const previousBlock = getNewestBlock();
+    const newBlockIndex = previousBlock.index + 1;
+    const newtimestamp = new Date().getTime() / 1000;
+    const newHash = createHash(
+        newBlockIndex,
+        previousBlock.hash,
+        newtimestamp,
+        data
+    );
+    const newBlock = new Block(
+        newBlockIndex,
+        newHash,
+        previousBlock.hash,
+        newtimestamp,
+        data
+    );
+    addBlockToChain(newBlock);
+    return newBlock;
 };
 
 // Get any block's hash
 const getBlockHash = block =>
-  createHash(block.index, block.previousHash, block.timestamp, block.data);
+    createHash(block.index, block.previousHash, block.timestamp, block.data);
 
 // Check if the structure of the Block and it's types are what they should be
 const isBlockStructureValid = block => {
-  return (
-    typeof block.index === "number" &&
+    return (
+        typeof block.index === "number" &&
     typeof block.hash === "string" &&
     typeof block.previousHash === "string" &&
     typeof block.timestamp === "number" &&
     typeof block.data === "string"
-  );
+    );
 };
 
 // Valiate new blocks
 const isBlockValid = (newBlock, oldBlock) => {
-  // Check if the structure of the new block is correct
-  if (!isBlockStructureValid(newBlock)) {
-    return false;
-  }
-  // Check if the index of the new block is greater than the old block's index
-  if (oldBlock.index + 1 !== newBlock.index) {
-    return false;
+    // Check if the structure of the new block is correct
+    if (!isBlockStructureValid(newBlock)) {
+        return false;
+    }
+    // Check if the index of the new block is greater than the old block's index
+    if (oldBlock.index + 1 !== newBlock.index) {
+        return false;
     // Check if the new block's previous hash is the same as the old block's hash
-  } else if (oldBlock.hash !== newBlock.previousHash) {
-    return false;
+    } else if (oldBlock.hash !== newBlock.previousHash) {
+        return false;
     // Check if the new block's hash is the same as the hash taht we calculate
-  } else if (getBlockHash(newBlock) !== newBlock.hash) {
-    return false;
-  }
-  return true;
+    } else if (getBlockHash(newBlock) !== newBlock.hash) {
+        return false;
+    }
+    return true;
 };
 
 // Check if the chain is valid
 const isChainValid = foreignChain => {
-  const isGenesisValid = block => {
-    return JSON.stringify(block) === JSON.stringify(genesisBlock);
-  };
-  // Check if the genesis block is the same in our chain and theirs
-  if (!isGenesisValid(foreignChain[0])) {
-    return false;
-  }
-  // Validate each block from the other blockchain
-  for (let i = 1; i < foreignChain.length; i++) {
-    if (!isBlockValid(foreignChain[i], foreignChain[i - 1])) {
-      return false;
+    const isGenesisValid = block => {
+        return JSON.stringify(block) === JSON.stringify(genesisBlock);
+    };
+    // Check if the genesis block is the same in our chain and theirs
+    if (!isGenesisValid(foreignChain[0])) {
+        return false;
     }
-  }
-  return true;
+    // Validate each block from the other blockchain
+    for (let i = 1; i < foreignChain.length; i++) {
+        if (!isBlockValid(foreignChain[i], foreignChain[i - 1])) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Replace Chain
 const replaceChain = newChain => {
-  if (isChainValid(newChain) && newChain.length > getBlockchain().length) {
-    blockchain = newChain;
-    return true;
-  }
-  return false;
+    if (isChainValid(newChain) && newChain.length > getBlockchain().length) {
+        blockchain = newChain;
+        return true;
+    }
+    return false;
 };
 
 // Add block to chain
 const addBlockToChain = newBlock => {
-  if (isBlockValid(newBlock, getNewestBlock())) {
-    blockchain.push(newBlock);
-    return true;
-  } else {
-    return false;
-  }
+    if (isBlockValid(newBlock, getNewestBlock())) {
+        blockchain.push(newBlock);
+        return true;
+    } else {
+        return false;
+    }
 };
 
 module.exports = {
-  getBlockchain,
-  createNewBlock,
-  getNewestBlock,
-  isBlockStructureValid,
-  addBlockToChain,
-  replaceChain
+    getBlockchain,
+    createNewBlock,
+    getNewestBlock,
+    isBlockStructureValid,
+    addBlockToChain,
+    replaceChain
 };
--- src/p2p.js
+++ src/p2p.js
@@ -1,12 +1,12 @@
-const Blockchain = require("./blockchain"),
-  WebSocket = require("ws");
+const Blockchain = require("./blockchain");
+const WebSocket = require("ws");
 
 const {
-  getNewestBlock,
-  isBlockStructureValid,
-  addBlockToChain,
-  replaceChain,
-  getBlockchain
+    getNewestBlock,
+    isBlockStructureValid,
+    addBlockToChain,
+    replaceChain,
+    getBlockchain
 } = Blockchain;
 
 // We need to save the sockets somewhere
@@ -21,33 +21,34 @@ const BLOCKCHAIN_RESPONSE = "BLOCKCHAIN_RESPONSE";
 // Message Creators
 
 const getLatest = () => {
-  return {
-    type: GET_LATEST,
-    data: null
-  };
+    return {
+        type: GET_LATEST,
+        data: null
+    };
 };
 
 const getAll = () => {
-  return {
-    type: GET_ALL,
-    data: null
-  };
+    return {
+        type: GET_ALL,
+        data: null
+    };
 };
 
 const blockchainResponse = data => {
-  return {
-    type: BLOCKCHAIN_RESPONSE,
-    data
-  };
+    return {
+        type: BLOCKCHAIN_RESPONSE,
+        data
+    };
 };
 
 // Start the P2P Server
 const startP2PServer = port => {
-  const server = new WebSocket.Server({ port });
-  server.on("connection", ws => {
-    initConnection(ws);
-  });
-  console.log(`Nomad Coin P2P Server Running on port ${port} ✅`);
+    const server = new WebSocket.Server({ port });
+    server.on("connection", ws => {
+        initConnection(ws);
+    });
+    // eslint-disable-next-line
+    console.log(`Nomad Coin P2P Server Running on port ${port} ✅`);
 };
 
 // Getting the sockets
@@ -55,108 +56,108 @@ const getSockets = () => sockets;
 
 // This fires everytime we add a new socket
 const initConnection = socket => {
-  sockets.push(socket);
-  socketMessageHandler(socket);
-  sendMessage(socket, getLatest());
+    sockets.push(socket);
+    socketMessageHandler(socket);
+    sendMessage(socket, getLatest());
 };
 
 // We use this to add peers
 const connectToPeers = newPeer => {
-  const ws = new WebSocket(newPeer);
-  ws.on("open", () => {
-    initConnection(ws);
-  });
+    const ws = new WebSocket(newPeer);
+    ws.on("open", () => {
+        initConnection(ws);
+    });
 };
 
 // Parsing string to JSON
 const parseMessage = data => {
-  try {
-    return JSON.parse(data);
-  } catch (e) {
-    console.log("Error parsing message");
-    return null;
-  }
+    try {
+        return JSON.parse(data);
+    } catch (e) {
+        return null;
+    }
 };
 
 // Here we are gonna handle all the messages that our sockets get
 const socketMessageHandler = ws => {
-  ws.on("message", data => {
-    const message = parseMessage(data);
-    // Check if we get an empty message
-    if (message === "null") {
-      return;
-    }
-    switch (message.type) {
-      case GET_LATEST:
-        sendMessage(ws, returnLatest());
-        break;
-      case GET_ALL:
-        sendMessage(ws, returnAll());
-        break;
-      case BLOCKCHAIN_RESPONSE:
-        const receivedBlocks = message.data;
-        // If the blockchain answers with no blocks break
-        if (receivedBlocks === null) {
-          break;
+    ws.on("message", data => {
+        const message = parseMessage(data);
+        // Check if we get an empty message
+        if (message === "null") {
+            return;
         }
-
-        handleBlockchainResponse(receivedBlocks);
-        break;
-    }
-  });
+        const receivedBlocks = message.data;
+        switch (message.type) {
+        case GET_LATEST:
+            sendMessage(ws, returnLatest());
+            break;
+        case GET_ALL:
+            sendMessage(ws, returnAll());
+            break;
+        case BLOCKCHAIN_RESPONSE:
+            
+            // If the blockchain answers with no blocks break
+            if (receivedBlocks === null) {
+                break;
+            }
+            handleBlockchainResponse(receivedBlocks);
+            break;
+        }
+    });
 };
 
 const sendMessage = (ws, message) => ws.send(JSON.stringify(message));
 
 const sendMessageToAll = message =>
-  sockets.forEach(socket => sendMessage(socket, message));
+    sockets.forEach(socket => sendMessage(socket, message));
 
 const returnLatest = () => blockchainResponse([getNewestBlock()]);
 
 const returnAll = () => blockchainResponse(getBlockchain());
 
 const handleBlockchainResponse = receivedBlocks => {
-  // Check if the blockchain size is bigger than zero
-  if (receivedBlocks.length === 0) {
-    return;
-  }
-  const latestBlockReceived = receivedBlocks[receivedBlocks.length - 1];
-  // Validate the latest block structure on the chain
-  if (!isBlockStructureValid(latestBlockReceived)) {
-    return;
-  }
-  // Get the newest block from the blockchain
-  const newestBlock = getNewestBlock();
-  /*
+    // Check if the blockchain size is bigger than zero
+    if (receivedBlocks.length === 0) {
+        return;
+    }
+    const latestBlockReceived = receivedBlocks[receivedBlocks.length - 1];
+    // Validate the latest block structure on the chain
+    if (!isBlockStructureValid(latestBlockReceived)) {
+        return;
+    }
+    // Get the newest block from the blockchain
+    const newestBlock = getNewestBlock();
+    /*
     Check if the index of the block we received is greater than the newest block in our blockchain
     This means that our blockchain is behind
    */
-  if (latestBlockReceived.index > newestBlock.index) {
+    if (latestBlockReceived.index > newestBlock.index) {
     /* 
       Check if the received block has the hash of hour newest block in his 'previousHash'.
       This will mean that our blockchain is only one block behind
     */
-    if (newestBlock.hash === latestBlockReceived.previousHash) {
-      // If we are only one block behind all we have to do is add it to our chain
-      addBlockToChain(latestBlockReceived);
-      // If we only got one block that is not only one block behind we have to get the whole blockchain
-    } else if (receivedBlocks.length === 1) {
-      // Send message to all sockets to get our blockchain
-      sendMessageToAll(getAll());
-    } else {
-      /* 
+        if (newestBlock.hash === latestBlockReceived.previousHash) {
+            // If we are only one block behind all we have to do is add it to our chain
+            addBlockToChain(latestBlockReceived);
+            // If we only got one block that is not only one block behind we have to get the whole blockchain
+        } else if (receivedBlocks.length === 1) {
+            // Send message to all sockets to get our blockchain
+            sendMessageToAll(getAll());
+        } else {
+            /* 
         If we get more than one block and our blockchain is behind,
         we will just replace our blockchain with the longer one that we just received
       */
-      replaceChain(receivedBlocks);
-    }
-  } else {
+            replaceChain(receivedBlocks);
+        }
+    } else {
     // If we receive a blockchain but we are not behind it, we do nothing.
-    return;
-  }
+        return;
+    }
 };
 
 module.exports = {
-  startP2PServer,
-  connectToPeers
+    startP2PServer,
+    connectToPeers,
+    getSockets
 };
